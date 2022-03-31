7aa598f50268a4632756305531d6308482a4e297
Alirez.af <alirezafaramarze56@gmail.com>
2022-03-31 20:31:09 +0430

    -a

---
+++ src/components/Home/Product/Product.js
@@ -0,0 +1,137 @@
+
+// Style
+import "./Product.css";
+
+// img
+import image from "../../../img/image.png"
+
+
+const Product = () => {
+    return ( 
+
+        <section className="d-flex row product">
+    
+            <h3 className="title-product">نمونه کارهای ما </h3>
+    
+            <div className="col-12 col-md-6 col-lg-4">
+    
+                <a href="/">
+                    <div className="d-flex row div-Product">
+    
+                        <div className="p-0">
+                            <img src={image} className="img-product" alt="image"/>
+                        </div>
+    
+                        <div className="cption">
+                            <h4>faramarzi</h4>
+                            <p>personal Web</p>
+                        </div>
+    
+                    </div>
+                </a>
+    
+            </div>
+    
+            <div className="col-12 col-md-6 col-lg-4">
+    
+                <a href="/">
+                    <div className="d-flex row div-Product">
+    
+                        <div className="p-0">
+                            <img src={image} className="img-product" alt="image"/>
+                        </div>
+    
+                        <div className="cption">
+                            <h4>faramarzi</h4>
+                            <p>personal Web</p>
+                        </div>
+    
+                    </div>
+                </a>
+    
+            </div>
+    
+            <div className="col-12 col-md-6 col-lg-4">
+    
+                <a href="/">
+                    <div className="d-flex row div-Product">
+    
+                        <div className="p-0">
+                            <img src={image} className="img-product" alt="image"/>
+                        </div>
+    
+                        <div className="cption">
+                            <h4>faramarzi</h4>
+                            <p>personal Web</p>
+                        </div>
+    
+                    </div>
+                </a>
+    
+            </div>
+    
+            <div className="col-12 col-md-6 col-lg-4">
+    
+                <a href="/">
+                    <div className="d-flex row div-Product">
+    
+                        <div className="p-0">
+                            <img src={image} className="img-product" alt="image"/>
+                        </div>
+    
+                        <div className="cption">
+                            <h4>faramarzi</h4>
+                            <p>personal Web</p>
+                        </div>
+    
+                    </div>
+                </a>
+    
+            </div>
+    
+            <div className="col-12 col-md-6 col-lg-4">
+    
+                <a href="/">
+                    <div className="d-flex row div-Product">
+    
+                        <div className="p-0">
+                            <img src={image} className="img-product" alt="image"/>
+                        </div>
+    
+                        <div className="cption">
+                            <h4>faramarzi</h4>
+                            <p>personal Web</p>
+                        </div>
+    
+                    </div>
+                </a>
+    
+            </div>
+    
+            <div className="col-12 col-md-6 col-lg-4">
+    
+                <a href="/">
+                    <div className="d-flex row div-Product">
+    
+                        <div className="p-0">
+                            <img src={image} className="img-product" alt="image"/>
+                        </div>
+    
+                        <div className="cption">
+                            <h4>faramarzi</h4>
+                            <p>personal Web</p>
+                        </div>
+    
+                    </div>
+                </a>
+    
+            </div>
+    
+    
+    
+        </section>
+    
+     );
+}
+ 
+export default Product;
