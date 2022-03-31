78e11c5874f9e751e984f85b56b0c34d5f6ccbbc
mmf <mohammadmahdifaramarzi14@gmail.com>
2022-03-31 17:58:30 +0430

    Introduction section created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+
+// img
+import Introduction from './Introduction/Introduction';
+
 const Home = () => {
     return (
         <div>
+            <Introduction />
         </div>
     );
 };
--- src/components/Layouts/MainLayout.js
+++ src/components/Layouts/MainLayout.js
@@ -9,7 +9,7 @@ import "./MainLayout.css";
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className='container'>
             <svg className='vector1' width="1135" height="681" viewBox="0 0 1135 681" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <g filter="url(#filter0_f_38_864)">
                     <path d="M227.959 183.5C98.3588 180.7 111.959 97.3333 134.959 56V4L1151.96 -34C1168.13 145.333 1190.76 491.6 1151.96 442C1103.46 380 820.959 497.5 686.459 552.5C551.959 607.5 610.959 373 603.959 297.5C596.959 222 389.959 187 227.959 183.5Z" fill="url(#paint0_linear_38_864)" fill-opacity="0.5" />
